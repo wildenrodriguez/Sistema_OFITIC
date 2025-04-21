1dd6d31fe0e3038a72ba5b76aaa54245dcd6cb54
lz2712 <leizeraponte2020@gmail.com>
2025-04-21 04:24:45 -0400

    instalando libreria de DataTable

--- view/Componentes/head.php
+++ view/Componentes/head.php
@@ -16,6 +16,10 @@
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
     />
+    <link
+      rel="stylesheet"
+      href="assets/DataTables/datatables.css"
+    />
     <script>
       const htmlElement = document.documentElement;
 
@@ -29,5 +33,6 @@
     </script>
     <script defer src="assets/js/main.js"></script>
     <script defer src="assets/js/utils.js"></script>
-    <script defe src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script defer src="assets/js/DataTables/datatables.js"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </head>
