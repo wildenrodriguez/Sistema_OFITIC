8322fdbd3f697f882bb19d6d53d1f988fc5cfbf2
lz2712 <leizeraponte2020@gmail.com>
2025-04-21 17:08:37 -0400

    Bitacora (Arcihvo con la clase

---
+++ model/bitacora.php
@@ -0,0 +1,135 @@
+<?php
+require_once "model/conexion.php";
+class Bitacora extends Conexion{
+
+    private $id;
+    private $usuario;
+    private $modulo;
+    private $accion;
+    private $fecha;
+    private $hora;
+
+    public function __construct(){
+        
+        $this->conex = new Conexion();
+        $this->conex = $this->conex->Conex();
+    }
+
+    public function set_id($id){
+        $this->id = $id;
+    };
+
+    public function set_usuario($usuario){
+        $this->usuario = $usuario;
+    };
+    public function set_modulo($modulo){
+        $this->modulo = $modulo;
+    };
+    public function set_accion($accion){
+        $this->accion = $accion;
+    };
+    public function set_fecha($fecha){
+        $this->fecha = $fecha;
+    };
+    public function set_hora($hora){
+        $this->hora = $hora;
+    };
+
+    private function Validar(){
+        $dato = [];
+
+        try {
+            $query = "SELECT * FROM bitacora WHERE id = :id";
+            
+            $stm = $this->conex->prepare($query);
+            $stm->bindParam(":id", $this->id);
+            $stm->execute();
+
+            if($stm->rowCount() > 0){
+                $dato['arreglo'] = $stm->fetch(PDO::FETCH_ASSOC);
+                $dato['bool'] = 1;
+            } else {
+                $dato['bool'] = 0;
+            }
+            
+        } catch (PDOException $e) {
+            $dato['error'] = $e->getMenssage();
+        }
+
+        return $dato;
+    }
+
+    private function Registrar(){
+        $dato = [];
+        $bool = $this->Validar()
+
+        if($bool['bool'] == 0){
+
+        try {
+            $query = "INSERT INTO bitacora (id, usuario, modulo, accion, fecha, hora)
+            VALUES (NULL, :usuario, :modulo, :accion, :fecha, :hora)";
+            
+            $stm = $this->conex->prepare($query);
+            $stm->bindParam(":usuario", $this->usuario);
+            $stm->bindParam(":modulo", $this->modulo);
+            $stm->bindParam(":accion", $this->accion);
+            $stm->bindParam(":fecha", $this->fecha);
+            $stm->bindParam(":hora", $this->hora);
+            $stm->execute();
+            $dato['resultado'] = "registrar";
+            $dato['mensaje'] = "Se registro con éxito";
+        } catch (PDOException $e) {
+            $dato['resultado'] = "error";
+            $dato['mensaje'] = $e->getMenssage();
+        }
+    } else {
+        $dato['resultado'] = "error";
+        $dato['mensaje'] = "Registro duplicado";
+    }
+        return $dato;
+    };
+
+
+    private function Consultar(){
+        $dato = [];
+
+        try {
+            $query = "SELECT * FROM bitacora";
+            
+            $stm = $this->conex->prepare($query);
+            $stm->execute();
+            $dato['arreglo'] = $stm->fetchAll(PDO::FETCH_ASSOC)
+        } catch (PDOException $e) {
+            $dato['error'] = $e->getMenssage();
+        }
+
+        return $dato;
+    }
+
+    public function Transaccion($peticion){
+
+        switch($peticion['peticion']){
+
+            case 'registrar':
+                return $this->Registrar();
+            
+            case 'consultar':
+                return $this->Consultar();
+
+            default:
+                return "Operacion: ".$peticion['peticion']." no valida";
+
+
+
+        }
+
+    }
+}
+
+
+
+
+
+
+
+?>
