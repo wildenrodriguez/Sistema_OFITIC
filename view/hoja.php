c23c92faddb55ca3b1e8b46a291a622535ea1201
Maribok <bokorarcangel447@gmail.com>
2025-05-04 22:55:40 -0400

    Mensaje de Prueba #2

--- view/hoja.php
+++ view/hoja.php
@@ -13,7 +13,7 @@
           <li class="breadcrumb-item active"><a><?php echo $titulo ?></a></li>
         </ol>
       </nav>
-    </div><!-- End Page Title -->
+    </div>                                 <!-- End Page Title -->
 
     <?php
     switch ($datos_hoja["tipo_s"]) {
