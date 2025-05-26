2b5b31a725b5b79708ad9b618b25951c474d9d8b
Jorge Cabrera <126417840+JorgeCabrera2003@users.noreply.github.com>
2025-05-25 21:28:10 -0400

    Cambios mapas

--- view/ayuda.php
+++ view/ayuda.php
@@ -31,13 +31,42 @@
               </div>
 
               <div class=" row">
-
+                <!-- Comienzo cards -->
                 <div class="card m-2">
-
                   <div class="card-body">
                     <h5 class="card-title">Como iniciar sesion</h5>
                     <p class="card-title">Para poder iniciar sesion tenemos que entrar en el sistema, ya estando adentro del sistema aparecera </p>
-                    
+                  </div>
+                  <div id="imageCarousel" class="carousel slide text-center" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <img src="assets/img/1.jpg" class="d-block w-50" alt="First slide">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="assets/img/2.jpeg" class="d-block w-50" alt="Second slide">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="assets/img/3.jpeg" class="d-block w-50" alt="Third slide">
+                      </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#imageCarousel" data-bs-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#imageCarousel" data-bs-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Next</span>
+                    </button>
+                  </div>
+                  <p class="card-text"><small class="text-body-secondary">Ultima modificacion 3 minutos atras</small></p>
+                </div>
+                <!-- Fin cards -->
+                <div class="card m-2">
+
+                  <div class="card-body">
+                    <h1 class="card-title">Dashboard</h1>
+                    <p class="card-title">Para poder iniciar sesion tenemos que entrar en el sistema, ya estando adentro del sistema aparecera </p>
+
                   </div>
                   <div id="imageCarousel" class="carousel slide text-center" data-bs-ride="carousel">
                     <div class="carousel-inner">
@@ -63,17 +92,38 @@
                   <p class="card-text"><small class="text-body-secondary">Ultima modificacion 3 minutos atras</small></p>
                 </div>
 
-                
+
 
                 <?php if ($datos["rol"] == "SUPERUSUARIO" or $datos["rol"] == "Técnico") { ?>
 
-                  <div class="col-md-6 col-lg-4 mb-4 video-container">
-                    <video class="img-fluid" controls>
-                      <source src="img/videos/servicios.mp4" type="video/mp4">
-                    </video>
-                    <div class="caption-container">
-                      <p class="gallery-caption">Uso del modulo servicios</p>
+                  <div class="card m-2">
+                    <div class="card-body">
+                      <h1 class="card-title">Servicio tecnico</h1>
+                      <p class="card-title">Para poder iniciar sesion tenemos que entrar en el sistema, ya estando adentro del sistema aparecera </p>
+
                     </div>
+                    <div id="imageCarousel" class="carousel slide text-center" data-bs-ride="carousel">
+                      <div class="carousel-inner">
+                        <div class="carousel-item active">
+                          <img src="assets/img/1.jpg" class="d-block w-50" alt="First slide">
+                        </div>
+                        <div class="carousel-item">
+                          <img src="assets/img/2.jpeg" class="d-block w-50" alt="Second slide">
+                        </div>
+                        <div class="carousel-item">
+                          <img src="assets/img/3.jpeg" class="d-block w-50" alt="Third slide">
+                        </div>
+                      </div>
+                      <button class="carousel-control-prev" type="button" data-bs-target="#imageCarousel" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                      </button>
+                      <button class="carousel-control-next" type="button" data-bs-target="#imageCarousel" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                      </button>
+                    </div>
+                    <p class="card-text"><small class="text-body-secondary">Ultima modificacion 3 minutos atras</small></p>
                   </div>
 
                 <?php } ?>
